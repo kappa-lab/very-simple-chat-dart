a7158420661d28d0733451cbf78e3fbb065fbf0c
yamagami kenichi <gamiken@kappa-lab.com>
2021-12-05 20:41:28 +0900

    server

---
+++ server.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:ffi';
+import "dart:io";
+import "dart:async";
+
+import 'dart:typed_data';
+
+const int PORT = 8080;
+
+void startServer() {
+  ServerSocket.bind("localhost", PORT)
+    ..then((ServerSocket ss) {
+      print("[Server Start]");
+
+      ss.listen((socket) => initConnection(socket));
+    });
+}
+
+void initConnection(Socket socket) {
+  print("[Init Connection] ---> ");
+  var greetingMessage = {"connId": 1, "roomId": 1, "member": []};
+  socket.add(jsonEncode(greetingMessage).codeUnits);
+
+  socket.listen(
+    (data) {
+      var newData = Uint8List(data.length + 2)
+        ..setAll(0, "-".codeUnits)
+        ..setAll(1, data)
+        ..setAll(data.length, "]".codeUnits);
+      socket.add(newData);
+    },
+    onDone: () => {print("on done")},
+  );
+}
